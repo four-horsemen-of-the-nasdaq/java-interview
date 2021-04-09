d5ce0cd5c0f4eae445be8f7ea042b83436fed5c1
four-horsemen-of-the-nasdaq <72942802+four-horsemen-of-the-nasdaq@users.noreply.github.com>
2021-04-09 14:42:32 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,55 @@
+package com.github.archarithms;
+
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class App
+{
+  public static String convertToTitleCase(final String inpStr)
+  {
+    {
+      //check for null
+      if(inpStr == null) {
+        throw new NullPointerException();
+      }
+      //make string lower case
+      String input = inpStr.toLowerCase();
+      //remove all non alpha numeric
+      input = input.replaceAll("[^a-zA-Z0-9]"," ");
+      //remove excess whitespace
+      input = input.replaceAll("\\s+"," ");
+      //Captialize Words
+      char[] charArray = input.toCharArray();
+      boolean foundSpace = true;
+      for(int i=0; i < charArray.length; i++)
+      {
+        if(Character.isLetter(charArray[i]) && foundSpace)
+        {
+          charArray[i] = Character.toUpperCase(charArray[i]);
+          foundSpace = false;
+        }
+        if(charArray[i]==' ')
+        {
+          foundSpace=true;
+        }
+      }
+      //return completed string
+      return String.valueOf(charArray);
+    }
+
+  }
+
+  public static String convertUnixToDateString(final Long inpUnixSeconds)
+  {
+    //check for null
+    if(inpUnixSeconds==null)
+    {
+      throw new NullPointerException();
+    }
+    else return LocalDateTime.ofInstant(Instant.ofEpochSecond(inpUnixSeconds),
+            ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("MMMM d, YYYY"));
+  }
+}
